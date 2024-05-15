903afddd3f32314a79f6216d9d7934c5a6ddb3ac
Sara <Sara.Ginsburg.ny@gmail.com>
2024-05-15 14:30:25 -0400

    create listing api route

---
+++ api/models/listing.model.js
@@ -0,0 +1,51 @@
+import mongoose, { trusted } from 'mongoose';
+
+const listingSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    field: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    featuredWork: {
+      type: Array,
+      required: true,
+    },
+    initialServiceCall: {
+      type: Number,
+      required: true,
+    },
+    locationServed: {
+      type: Array,
+      required: true,
+    },
+    backgroundChecked: {
+      type: Boolean,
+      required: true,
+    },
+    numberOfEmployees: {
+      type: Number,
+      required: true,
+    },
+    yearsInBusiness: {
+      type: Number,
+      required: true,
+    },
+    userRef: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Listing = mongoose.model('Listing', listingSchema);
+
+export default Listing;
