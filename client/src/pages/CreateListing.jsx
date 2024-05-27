768e28f6bc51bf789610093f2ef5d19c652525df
Sara <Sara.Ginsburg.ny@gmail.com>
2024-05-27 13:34:24 -0400

    complete upload images for featured work

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,19 +1,101 @@
 import { useState } from 'react';
-
-const handleChange = () => {};
+import {
+  getDownloadURL,
+  getStorage,
+  uploadBytesResumable,
+  ref,
+} from 'firebase/storage';
+import { app } from '../firebase';
 
 const CreateListing = () => {
-  // // const [formData, setFormData] = useState({
-  //   name: '',
-  //   field: '',
-  //   description: '',
-  //   featuredWork: [],
-  //   initialServiceCall: 0,
-  //   locationServed: [],
-  //   backgroundChecked: false,
-  //   numberOfEmployees: 0,
-  //   yearsInBusiness: 0,
-  // };
+  const [files, setFiles] = useState([]);
+  const [formData, setFormData] = useState({
+    name: '',
+    field: '',
+    description: '',
+    featuredWork: [],
+    initialServiceCall: 0,
+    locationServed: [],
+    backgroundChecked: false,
+    numberOfEmployees: 0,
+    yearsInBusiness: 0,
+  });
+  const [imageUploadError, setImageUploadError] = useState('');
+  const [uploading, setUploading] = useState(false);
+
+  const handleChange = (e) => {
+    const { id, value, type, checked } = e.target;
+    if (type === 'checkbox') {
+      setFormData((prevState) => ({ ...prevState, [id]: checked }));
+    } else {
+      setFormData((prevState) => ({ ...prevState, [id]: value }));
+    }
+  };
+
+  const handleImagesSubmit = (e) => {
+    if (files.length === 0) {
+      setImageUploadError('No images selected');
+      return;
+    }
+
+    if (files.length > 0 && files.length + formData.featuredWork.length < 11) {
+      setUploading(true);
+      setImageUploadError(false);
+      const promises = [];
+      for (let i = 0; i < files.length; i++) {
+        promises.push(storeImage(files[i]));
+      }
+
+      Promise.all(promises)
+        .then((urls) => {
+          setFormData((prevState) => ({
+            ...prevState,
+            featuredWork: prevState.featuredWork.concat(urls),
+          }));
+          setImageUploadError('');
+          setUploading(false);
+        })
+        .catch((error) => {
+          setImageUploadError('Image upload failed (2 mb max per image)');
+          setUploading(false);
+        });
+    } else {
+      setImageUploadError('You can upload only 10 images of featured work');
+      setUploading(false);
+    }
+  };
+
+  const storeImage = async (file) => {
+    return new Promise((resolve, reject) => {
+      const storage = getStorage(app);
+      const fileName = new Date().getTime() + file.name;
+      const storageRef = ref(storage, fileName);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+      uploadTask.on(
+        'state_changed',
+        (snapshot) => {
+          const progress =
+            (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        },
+        (error) => {
+          reject(error);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+            resolve(downloadURL);
+          });
+        }
+      );
+    });
+  };
+
+  const handleRemoveImage = (idx) => {
+    setFormData((prevState) => ({
+      ...prevState,
+      featuredWork: prevState.featuredWork.filter((_, i) => i !== idx),
+    }));
+    console.log('formData', formData);
+  };
 
   return (
     <main className='p-3 max-w-4xl mx-auto'>
@@ -125,14 +207,10 @@ const CreateListing = () => {
         </div>
 
         <div className='flex flex-col flex-1 gap-4 mx-10'>
-          <p className=' font-semibold text-slate-500'>
-            Images:
-            <span className='font-normal text-slate-500 ml-2'>
-              The first image will be a cover photo (max 6)
-            </span>
-          </p>
+          <p className=' font-semibold text-slate-500'>Featured Projects:</p>
           <div className=' flex gap-4'>
             <input
+              onChange={(e) => setFiles(e.target.files)}
               className='p-3 border border-customGreen  rounded-lg
                w-full text-slate-500'
               type='file'
@@ -140,10 +218,36 @@ const CreateListing = () => {
               accept='image/*'
               multiple
             />
-            <button className='p-3 text-customGreen border border-customGreen rounded-lg uppercase hover:shadow-lg disabled:opacity-70'>
-              Upload
+            <button
+              type='button'
+              disabled={uploading}
+              onClick={handleImagesSubmit}
+              className='p-3 text-customGreen border border-customGreen
+              rounded-lg uppercase hover:shadow-lg disabled:opacity-70'>
+              {uploading ? 'uploading...' : 'upload'}
             </button>
           </div>
+          <p className='text-orange-500 text-sm'>
+            {imageUploadError && imageUploadError}
+          </p>
+          {formData.featuredWork.length > 0 &&
+            formData.featuredWork.map((url, idx) => (
+              <div
+                key={url}
+                className='flex justify-between p-3 border items-center'>
+                <img
+                  src={url}
+                  alt='featured work image'
+                  className='w-40 h-40 object-cover rounded-lg'
+                />
+                <button
+                  type='button'
+                  onClick={() => handleRemoveImage(idx)}
+                  className='p-3 text-orange-500 text-sm rounded-lg uppercase hover:opacity-60'>
+                  Delete
+                </button>
+              </div>
+            ))}
           <button className=' rounded-lg p-3 bg-slate-500 text-white uppercase hover:opacity-80 disabled:opacity-50'>
             Create Listing
           </button>
