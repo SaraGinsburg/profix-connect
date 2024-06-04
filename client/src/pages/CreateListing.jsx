9fde925ca971e1f0c1a76b6141d1d1170442ac6f
Sara <Sara.Ginsburg.ny@gmail.com>
2024-06-04 19:11:14 -0400

    complete CreateListing page - functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import {
   getDownloadURL,
   getStorage,
@@ -6,8 +6,11 @@ import {
   ref,
 } from 'firebase/storage';
 import { app } from '../firebase';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 const CreateListing = () => {
+  const { currentUser } = useSelector((state) => state.user);
   const [files, setFiles] = useState([]);
   const [formData, setFormData] = useState({
     name: '',
@@ -17,19 +20,38 @@ const CreateListing = () => {
     initialServiceCall: 0,
     locationServed: [],
     backgroundChecked: false,
-    numberOfEmployees: 0,
+    numberOfEmployees: 1,
     yearsInBusiness: 0,
   });
   const [imageUploadError, setImageUploadError] = useState('');
   const [uploading, setUploading] = useState(false);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     const { id, value, type, checked } = e.target;
-    if (type === 'checkbox') {
-      setFormData((prevState) => ({ ...prevState, [id]: checked }));
-    } else {
-      setFormData((prevState) => ({ ...prevState, [id]: value }));
+
+    setFormData((prevState) => ({
+      ...prevState,
+      [id]: type === 'checkbox' ? checked : value,
+    }));
+  };
+
+  const handleSelectChange = (e) => {
+    const options = e.target.options;
+    const selectedOptions = [];
+
+    for (let i = 0; i < options.length; i++) {
+      if (options[i].selected) {
+        selectedOptions.push(options[i].value);
+      }
     }
+    setFormData((prevState) => ({
+      ...prevState,
+      locationServed: selectedOptions,
+    }));
+    console.log('formData', formData);
   };
 
   const handleImagesSubmit = (e) => {
@@ -97,24 +119,51 @@ const CreateListing = () => {
     console.log('formData', formData);
   };
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      setError(false);
+      const res = await fetch('/api/listing/create', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ ...formData, userRef: currentUser._id }),
+      });
+      const data = await res.json();
+      setLoading(false);
+
+      if (data.success === false) {
+        setError(data.message);
+      }
+      navigate(`/listing/${data._id}`);
+      setError('');
+    } catch (error) {
+      setError(error.message);
+      setLoading(false);
+    }
+  };
   return (
     <main className='p-3 max-w-4xl mx-auto'>
       <h1 className='text-center my-7 text-3xl font-semibold text-slate-500'>
         Create a Listing
       </h1>
-      <form className='mx-3 flex flex-col sm:flex-row     gap-10 md:justify-between'>
+      <form
+        onSubmit={handleSubmit}
+        className='mx-3 flex flex-col sm:flex-row gap-10 md:justify-between'>
         <div className='flex flex-col  gap-2 flex-1'>
           <div className='flex'>
             <input
               type='text'
-              placeholder='Name'
+              placeholder='Business Name'
               className='focus:outline-none border p-3 rounded-lg flex-grow mr-2 w-3/4'
               id='name'
               maxLength='100'
-              minLength='10'
+              minLength='4'
               required
               onChange={handleChange}
-              // value={formData.name}
+              value={formData.name}
             />
             <input
               type='text'
@@ -122,10 +171,10 @@ const CreateListing = () => {
               className='focus:outline-none border p-3 rounded-lg w-1/4'
               id='field'
               maxLength='50'
-              minLength='10'
+              minLength='6'
               required
               onChange={handleChange}
-              // value={formData.field}
+              value={formData.field}
             />
           </div>
 
@@ -136,7 +185,7 @@ const CreateListing = () => {
             id='description'
             required
             onChange={handleChange}
-            // value={formData.description}
+            value={formData.description}
           />
 
           <div className='flex mx-3 my-4 gap-6 flex-wrap'>
@@ -148,17 +197,23 @@ const CreateListing = () => {
                 min='0'
                 max='300'
                 className=' accent-customGreen w-15 focus:outline-none border text-right border-customGreen text-slate-500 rounded-lg'
+                onChange={handleChange}
+                value={formData.initialServiceCall}
               />
-              <span className='text-sm text-slate-400'>$/service call</span>
+              <span className='text-sm text-slate-400'>
+                $ / initial consultation charge
+              </span>
             </div>
             <div className='flex gap-2  items-center'>
               <input
                 type='number'
                 required
                 id='yearsInBusiness'
-                min='0'
+                min='1'
                 max='30'
-                // default='0'
+                default='0'
+                onChange={handleChange}
+                value={formData.yearsInBusiness}
                 className=' accent-customGreen w-15 focus:outline-none border text-right border-customGreen text-slate-500 rounded-lg'
               />
               <span className='text-sm text-slate-400'>years in business</span>
@@ -168,7 +223,9 @@ const CreateListing = () => {
                 id='locationServed'
                 multiple
                 required
-                className='accent-customGreen border p-2 rounded-lg w-full focus:outline-none border-customGreen text-slate-500 h-48'>
+                className='accent-customGreen border p-2 rounded-lg w-full focus:outline-none border-customGreen text-slate-500 h-48'
+                onChange={handleSelectChange}
+                value={formData.locationServed}>
                 <option value='Brooklyn'>Brooklyn</option>
                 <option value='Manhattan'>Manhattan</option>
                 <option value='Queens'>Queens</option>
@@ -187,6 +244,8 @@ const CreateListing = () => {
                 min='1'
                 max='100'
                 className=' accent-customGreen w-15 focus:outline-none border text-right border-customGreen text-slate-500 rounded-lg'
+                onChange={handleChange}
+                value={formData.numberOfEmployees}
               />
               <span className='text-sm text-slate-400'>
                 number of employees
@@ -198,6 +257,8 @@ const CreateListing = () => {
                 id='backgroundChecked'
                 className=' accent-customGreen w-4 focus:outline-none border
                  p-3   rounded-lg'
+                onChange={handleChange}
+                checked={formData.backgroundChecked}
               />
               <span className='text-sm text-slate-400'>
                 employees background checked
@@ -248,10 +309,18 @@ const CreateListing = () => {
                 </button>
               </div>
             ))}
-          <button className=' rounded-lg p-3 bg-slate-500 text-white uppercase hover:opacity-80 disabled:opacity-50'>
-            Create Listing
+          <button
+            disabled={loading || uploading}
+            className=' rounded-lg p-3 bg-slate-500 text-white uppercase hover:opacity-80 disabled:opacity-50'>
+            {loading ? 'Creating' : 'Create Listing'}
           </button>
         </div>
+
+        {error ? (
+          <p className='text-orange-500 text-sm'>Error creating listing </p>
+        ) : (
+          ''
+        )}
       </form>
     </main>
   );
