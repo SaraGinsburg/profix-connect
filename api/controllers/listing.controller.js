2e647122dca14b0e4d04b23dec7cfa9aaf9293ee
Sara <Sara.Ginsburg.ny@gmail.com>
2024-07-01 16:06:00 -0400

    created updateListing api

--- api/controllers/listing.controller.js
+++ api/controllers/listing.controller.js
@@ -1,5 +1,6 @@
 import Listing from '../models/listing.model.js';
 import { errorHandler } from '../utils/error.js';
+import mongoose from 'mongoose';
 
 export const createListing = async (req, res, next) => {
   try {
@@ -24,6 +25,44 @@ export const deleteListing = async (req, res, next) => {
   try {
     await Listing.findByIdAndDelete(req.params.id);
     res.status(200).json('Listing has been deleted');
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const updateListing = async (req, res, next) => {
+  try {
+    const listing = await Listing.findById(req.params.id);
+
+    if (!listing) {
+      console.log('listing not found');
+      return next(errorHandler(404, 'listing not found!'));
+    }
+
+    if (listing.userRef !== req.user.id) {
+      console.log('unauthorized user');
+      return next(errorHandler(401, 'you can update your listings only!'));
+    }
+
+    const isValidObjectId = mongoose.Types.ObjectId.isValid(req.params.id);
+    if (!isValidObjectId) {
+      console.log('invalid objectId in the update request');
+      return next(errorHandler(400, 'Invalid ObjectId in the update request'));
+    }
+
+    try {
+      const updatedListing = await Listing.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        { new: true }
+      );
+
+      console.log('listing updated successfully');
+      res.status(200).json(updatedListing);
+    } catch (error) {
+      console.error('Error updating listing:', error);
+      next(error);
+    }
   } catch (error) {
     next(error);
   }
