7d1ec5cff31e50fb5ed7cf8a4013ca263acbec00
Sara <Sara.Ginsburg.ny@gmail.com>
2024-07-22 13:40:11 -0400

    Search page UI - wip

--- client/src/pages/Search.jsx
+++ client/src/pages/Search.jsx
@@ -1,6 +1,13 @@
 import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Search = () => {
+  const navigate = useNavigate();
+  const [sidebarData, setSidebarData] = useState({
+    searchTerm: ' ',
+    field: '',
+    location: '',
+  });
   const [searchTerm, setSearchTerm] = useState('');
   const [fields, setFields] = useState([]);
   const [selectedField, setSelectedField] = useState('');
@@ -8,6 +15,9 @@ const Search = () => {
   const [selectedLocation, setSelectedLocation] = useState('');
 
   useEffect(() => {
+    // const urlParams = new URLSearchParams();
+    // const urlParams = new URLSearchParams(location.search);
+
     const fetchFields = async () => {
       try {
         const res = await fetch('/api/listing/fields');
@@ -31,58 +41,68 @@ const Search = () => {
     fetchLocations();
   }, []);
 
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div className='flex flex-col md:flex-row'>
+    <div className='flex flex-col md:flex-row gap-8'>
       <div className='p-7 border-b-2 md:border-r-2 md:min-h-screen'>
-        <form>
-          <div className='flex items-center gap-2'>
-            <label className='text-slate-600'>Search Term: </label>
-            <input
-              type='text'
-              id='searchTerm'
-              placeholder='Search...'
-              className='border bg-slate-25 rounded-lg p-3 w-full focus:customGreen focus:outline-customGreen'
-              value={searchTerm}
-              onChange={(e) => setSearchTerm(e.target.value)}
-            />
+        <form onSubmit={handleSubmit}>
+          <div className='flex flex-col gap-11'>
+            <div className='flex items-center gap-2'>
+              <label className='font-semibold text-slate-500'>
+                Search Term:{' '}
+              </label>
+              <input
+                type='text'
+                id='searchTerm'
+                placeholder='Search...'
+                className='border bg-slate-25 rounded-lg p-3 w-full focus:customGreen focus:outline-customGreen'
+                value={searchTerm}
+                onChange={(e) => setSearchTerm(e.target.value)}
+              />
+            </div>
+            <div className='flex flex-wrap items-center gap-11 text-slate-500 bg-slate-25'>
+              <div className='flex gap-3 items-center'>
+                <label className='font-semibold'>Expertise:</label>
+                <select
+                  value={selectedField}
+                  onChange={(e) => setSelectedField(e.target.value)}
+                  className='border rounded-lg p-3 focus:outline-none focus:ring-1
+                   focus:ring-customGreen custom-select w-58'>
+                  <option value='' disabled>
+                    Select an Expertise
+                  </option>
+                  {fields.map((field) => (
+                    <option className='p-3' key={field} value={field}>
+                      {' '}
+                      {field}
+                    </option>
+                  ))}
+                </select>
+              </div>
+              <div className='flex gap-3 items-center'>
+                <label className='font-semibold'>Location:</label>
+                <select
+                  value={selectedLocation}
+                  onChange={(e) => setSelectedLocation(e.target.value)}
+                  className='border rounded-lg p-3 focus:outline-none focus:ring-1
+                   focus:ring-customGreen custom-select w-58'>
+                  <option value='' disabled className='px-9 '>
+                    Select a Location
+                  </option>
+                  {locations.map((location) => (
+                    <option className='p-3' key={location} value={location}>
+                      {location}
+                    </option>
+                  ))}
+                </select>
+              </div>{' '}
+            </div>
           </div>
-          <div className='flex items-center gap-4 text-slate-500 bg-slate-25'>
-            <label className='font-semibold'>Expertise:</label>
-            <select
-              value={selectedField}
-              onChange={(e) => setSelectedField(e.target.value)}
-              className='border rounded-lg p-3 focus:outline-none focus:ring-1
-              focus:ring-customGreen custom-select'>
-              <option value='' disabled>
-                Select an Expertise
-              </option>
-              {fields.map((field) => (
-                <option className='p-3' key={field}>
-                  {' '}
-                  {field}
-                </option>
-              ))}
-            </select>
 
-            <label className='font-semibold'>Location:</label>
-            <select
-              value={selectedLocation}
-              onChange={(e) => setSelectedLocation(e.target.value)}
-              className='border rounded-lg p-3 focus:outline-none focus:ring-1
-              focus:ring-customGreen custom-select'>
-              <option value='' disabled>
-                Select a Location
-              </option>
-              {locations.map((location) => (
-                <option className='p-3' key={location}>
-                  {' '}
-                  {location}
-                </option>
-              ))}
-            </select>
-          </div>
-
-          <button className='bg-slate-500 text-slate-200 p-3 rounded-lg uppercase hover:opacity-80'>
+          <button className='mt-20 bg-slate-400 text-slate-200 p-3 rounded-lg mx-auto w-full uppercase hover:opacity-80'>
             {' '}
             Search
           </button>
@@ -90,7 +110,9 @@ const Search = () => {
       </div>
 
       <div className=''>
-        <h1>listing results:</h1>
+        <h1 className='text-3xl font-semibold text-slate-500 border-b p-3 mt-5'>
+          Listings:
+        </h1>
       </div>
     </div>
   );
